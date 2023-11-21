9bdd969a2bf5daf3e88589eadcd3b89b412ac240
Dhruv1184 <138669833+Dhruv1184@users.noreply.github.com>
2023-11-21 10:35:00 +0530

    Add files via upload

---
+++ src/AppClass.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./App.css"
+// import imageData from "./Component/DataComponent";
+
+export default class AppClass extends Component{
+  
+render(props){
+  const imgData = this.props.myData();
+  console.log(imgData)
+  return(
+      <div>
+        <h1>Kalvium Gallary</h1>
+        <div>
+          {imgData.map((imaage, index)=>{
+            return(
+            <div key={index} >
+              <img src={imaage.img} alt="" />
+            </div>)
+          })}
+
+        </div>
+      </div>
+    
+  )
+}
+  // code here
+}
